595ce6d6f4068feaebb087c4fab84f52a28e14a2
oliverdelacruzm <122914414+oliverdelacruzm@users.noreply.github.com>
2023-04-13 09:56:02 -0400

    Added JavaScript interactivity

---
+++ resources/js/main.js
@@ -0,0 +1,22 @@
+const navListItems = document.querySelectorAll("#nav-link");
+const currentURL = window.location.href;
+
+navListItems.forEach(item => {
+
+    if (item.href === currentURL) {
+        item.style.textDecoration = 'underline';
+        item.style.textUnderlineOffset = '7px';
+    };
+
+    item.addEventListener('mouseover', () => {
+        item.style.textDecoration = 'underline';
+        item.style.textUnderlineOffset = '7px';
+    });
+    item.addEventListener('mouseout', () => {
+        if (item.href !== currentURL) {
+            item.style.textDecoration = 'none';
+            item.style.textUnderlineOffset = 'initial';
+        };
+    });
+
+});
